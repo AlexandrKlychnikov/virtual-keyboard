39126040ce927dc7d4d1fc5bb0bc2b89e7376cb3
Klychnikov Alexandr <klychnikov75@mail.ru>
2022-05-09 13:29:37 +0300

    feat: create handler for Backspace key

--- src/scripts/handle-click.js
+++ src/scripts/handle-click.js
@@ -1,5 +1,6 @@
 import keySet from './keyset';
 import changeLayout from './changeLayout';
+import handleBackspace from './handle-backspace';
 
 export default function handleClick(event, caseMode, pressedKeys, keyboard) {
   const { layout = 'lat' } = localStorage;
@@ -18,10 +19,14 @@ export default function handleClick(event, caseMode, pressedKeys, keyboard) {
       const text = keySet[event.code][layout][caseMode];
       screen.focus();
       screen.setRangeText(text, screen.selectionStart, screen.selectionEnd, 'end');
+    } else if (event.code === 'Backspace') {
+      handleBackspace();
     }
   } else if (!keySet[event.target.id].mod) {
     const text = keySet[event.target.id][layout][caseMode];
     screen.focus();
     screen.setRangeText(text, screen.selectionStart, screen.selectionEnd, 'end');
+  } else if (event.target.id === 'Backspace') {
+    handleBackspace();
   }
 }
