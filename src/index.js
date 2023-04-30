3cfa145342064b4f034159f775bd417761cfb767
Klychnikov Alexandr <klychnikov75@mail.ru>
2023-04-30 10:18:52 +0300

    feat: create class Keyboard and start form index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import headingComponent from './scripts/heading';
+import screenComponent from './scripts/screen';
+import Keyboard from './scripts/keyboard';
+import './styles/input.sass';
+
+const keyboard = new Keyboard();
+const screen = screenComponent();
+document.body.appendChild(headingComponent());
+document.body.appendChild(screen);
+document.body.appendChild(keyboard.node);
