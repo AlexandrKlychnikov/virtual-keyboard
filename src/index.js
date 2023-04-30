7bfbc10b6510f9a02bb0c59b07bde3c7e3bd9a24
Klychnikov Alexandr <klychnikov75@mail.ru>
2023-04-30 11:17:20 +0300

    feat: create component info, view of app completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import headingComponent from './scripts/heading';
 import screenComponent from './scripts/screen';
 import Keyboard from './scripts/keyboard';
+import infotextComponent from './scripts/infotext';
 import './styles/input.sass';
 
 const keyboard = new Keyboard();
@@ -8,3 +9,4 @@ const screen = screenComponent();
 document.body.appendChild(headingComponent());
 document.body.appendChild(screen);
 document.body.appendChild(keyboard.node);
+document.body.appendChild(infotextComponent());
